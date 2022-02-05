a19d58878d85cda0613efabbc9e9fc204cc939bb
ML <labrecquemegan@hotmail.com>
2022-02-05 09:28:37 -0500

    created schema for sql tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,34 @@ CREATE DATABASE employee_db
 USE employee_db
 
 -- creates a table for 
+CREATE TABLE Employees (
+  id INT NOT NULL,
+  firstName VARCHAR(30) NOT NULL,
+  LastName VARCHAR(30) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+);
+
+CREATE TABLE Engineer (
+  id INT NOT NULL,
+  firstName VARCHAR(30) NOT NULL,
+  LastName VARCHAR(30) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  github VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE Intern (
+  id INT NOT NULL,
+  firstName VARCHAR(30) NOT NULL,
+  LastName VARCHAR(30) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  school VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE Manager (
+  id INT NOT NULL,
+  firstName VARCHAR(30) NOT NULL,
+  LastName VARCHAR(30) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  officeNumber VARCHAR(30) NOT NULL,
+);
+
